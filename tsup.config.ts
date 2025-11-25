e355c871a34c15564958d98841ba9c0644dc721b
$ ReYansh <relish.reyansh.489@outlook.com>
2025-11-25 19:17:42 +0530

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import type { Options } from "tsup";
 
 export const tsup: Options = {
   clean: true,
-  dts: true,
+  dts: false, // disable DTS build to avoid Rollup error
   entry: ["src/index.ts"],
   format: "cjs",
   minify: false,
